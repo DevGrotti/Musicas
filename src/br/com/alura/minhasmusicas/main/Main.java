1cc23a5ea3717aa647384e4904ba5de9cd4066d7
DevGrotti <matheusgrottipereira@gmail.com>
2025-02-08 09:34:17 -0300

    Instanciando Musica e Podcast

--- src/br/com/alura/minhasmusicas/main/Main.java
+++ src/br/com/alura/minhasmusicas/main/Main.java
@@ -1,7 +1,35 @@
 package br.com.alura.minhasmusicas.main;
 
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
 public class Main {
     public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.reproduz();
+
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+
+        }
 
     }
 }
